fbaf1fbad286a54c9b6596cc3734b24277181671
ChrisWhisker <chrisworc@gmail.com>
2024-03-30 15:32:50 -0400

    Require first character to be D, d or a number

--- rollvalidator.cpp
+++ rollvalidator.cpp
@@ -2,7 +2,7 @@
 
 RollValidator::State RollValidator::validate(QString &input, int &pos) const
 {
-    static QRegularExpression rx("^(?!.*[Dd][Dd])[0-9dD+\\-*/]*$"); // Regular expression to allow digits and specified math operators
+    static QRegularExpression rx("^[0-9dD](?!.*[Dd][Dd])[0-9dD+\\-*/]*$"); // Regular expression to allow digits and specified math operators
     if (rx.match(input).hasMatch() || input.isEmpty()) {
         return Acceptable; // Accept if the input matches the regular expression or is empty
     } else {
